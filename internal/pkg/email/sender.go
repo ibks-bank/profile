517faf7459c9b47220b573563f9b1649c9b2fc06
Igor Razumkov <irazumkov@ozon.ru>
2022-03-17 21:42:18 +0300

    use smpts instead of smtp

--- internal/pkg/email/sender.go
+++ internal/pkg/email/sender.go
@@ -1,6 +1,7 @@
 package email
 
 import (
+	"crypto/tls"
 	"net/smtp"
 
 	"github.com/ibks-bank/profile/internal/pkg/errors"
@@ -24,14 +25,44 @@ func NewSender(from, pass, serverHost, serverPort string) *sender {
 }
 
 func (s *sender) Send(to, code string) error {
-	auth := loginAuth(s.username, s.password)
-	err := smtp.SendMail(
-		s.smtpServerHost+":"+s.smtpServerPort,
-		auth,
-		s.username, []string{to}, []byte(code),
-	)
+	conn, err := tls.Dial("tcp", s.smtpServerHost+":"+s.smtpServerPort, &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         s.smtpServerHost,
+	})
 	if err != nil {
-		return errors.Wrap(err, "can't send email")
+		return errors.Wrap(err, "can't dial tls")
+	}
+
+	smtpClient, err := smtp.NewClient(conn, s.smtpServerHost)
+	if err != nil {
+		return errors.Wrap(err, "can't create smtp client")
+	}
+	defer smtpClient.Quit()
+
+	err = smtpClient.Auth(smtp.PlainAuth("", s.username, s.password, s.smtpServerHost))
+	if err != nil {
+		return errors.Wrap(err, "can't auth smtp")
+	}
+
+	err = smtpClient.Mail(s.username)
+	if err != nil {
+		return errors.Wrap(err, "can't do mail")
+	}
+
+	err = smtpClient.Rcpt(to)
+	if err != nil {
+		return errors.Wrap(err, "can't do rcpt")
+	}
+
+	w, err := smtpClient.Data()
+	if err != nil {
+		return errors.Wrap(err, "can't do data")
+	}
+	defer w.Close()
+
+	_, err = w.Write([]byte(code))
+	if err != nil {
+		return errors.Wrap(err, "can't write code")
 	}
 
 	return nil
